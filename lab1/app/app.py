82ce951a1e2046d194374f89a6cb03d167899024
The5teve <artem.fagradyan@mail.ru>
2020-10-24 14:53:28 +0300

    lab1

--- lab1/app/app.py
+++ lab1/app/app.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 application = app
 @app.route('/')
 def index():
-    return 'Hello. world'
+    msg= 'Hello'
+    return  render_template('index.html', msg=msg)
+
+@app.route('/posts')
+def posts():
+    return render_template('posts.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
